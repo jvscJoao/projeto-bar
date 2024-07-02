fd3453a54bba5e340138041e55402ff3b58799d9
jvscJoao <joaov1500@gmail.com>
2024-07-02 15:42:15 -0300

    first commit

---
+++ src/main/java/br/com/projeto/bar/projeto_bar/ProjetoBarApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.bar.projeto_bar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBarApplication.class, args);
+	}
+
+}
