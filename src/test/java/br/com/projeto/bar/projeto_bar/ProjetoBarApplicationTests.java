f559e2fd9e9f3e2598f78cddfc270ac6a3ed48ee
jvscJoao <joaov1500@gmail.com>
2025-01-10 12:42:00 -0300

    first commit

---
+++ src/test/java/br/com/projeto/bar/projeto_bar/ProjetoBarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.bar.projeto_bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
